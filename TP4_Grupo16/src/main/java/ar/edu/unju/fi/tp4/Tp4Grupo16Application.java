e52804a2f9dbb989c56dd2d39b00c8121111c27e
Nataniel <natanielarg@gmail.com>
2022-05-01 10:23:14 -0300

    primer commit

---
+++ TP4_Grupo16/src/main/java/ar/edu/unju/fi/tp4/Tp4Grupo16Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo16Application.class, args);
+	}
+
+}
---
+++ TP4_Grupo16/src/main/java/ar/edu/unju/fi/tp4/ejemplo/Principal.java
@@ -0,0 +1,10 @@
+package ar.edu.unju.fi.tp4.ejemplo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
